2dc60ba26c9b39f91c3ac5c09214125b08e23c3f
edu14cic <edu14cic@gmail.com>
2018-08-23 08:06:25 -0300

    aula3

---
+++ Aula2/scriptDEaula.R
@@ -0,0 +1,72 @@
+facil <- 2+2
+facil
+dificil <- 135/1.2
+dificil
+dificil * 3
+vetor1 <- c(1, 10 ,100)
+vetor1
+vetor2 <- c(2, 20, 200)
+vetor2
+vetor1 + vetor2
+vetor_concatenado <- c(vetor1,vetor2,5000)
+vetor_concatenado
+vetor_concatenado * (-2)
+vetor3 <- c(30, 60, 90, 120, 150 ,180)
+vetor3
+vetor1 * vetor3
+c(vetor1 * vetor3[1:3],vetor3[4:6])
+vetor1 <- c(vetor1,NA,NA,NA)
+vetor1
+vetor1 * vetor3
+
+vetorcom2 <- c(1,2)
+vetorcom3 <- c(10,20,30)
+vetorcom2
+vetorcom3
+vetorcom2 + vetorcom3
+
+meunome <- "Eduardo"
+meunome
+nomes_alunos <- c("ana","bia","carol",meunome)
+nomes_alunos
+
+nomes_alunos <- c("ana","bia","carol",Pedro)
+c(nomes_alunos,100)
+
+10 > 1
+1 > 10
+10 < 20
+10 <=12
+5 >= 3
+5 == 5
+5 == 10
+5 != 5
+5 != 10
+"ana" > "bia"
+"ana" < "bia"
+vetor_logico <- c(1 > 10, 12 > 1, 10 == 10 , "ana" == "ana", "ana" == "Ana")
+vetor_logico
+"a" < "A"
+meu_logico <- c(TRUE, T, FALSE, F)
+meu_logico
+T <- "tatu"
+meu_logico2 <- c(T)
+meu_logico2
+TRUE <- "tata"
+meu_logico2 <- c(T)
+texto_logico <- c(TRUE,"ana")
+texto_logico
+logico_numerico <- c(TRUE, FALSE, 100, 10000)
+logico_numerico
+rm(meu_logico2)
+ls()
+vetor3[5]
+vetor3[c(1,5)]
+grande <- 11:380
+grande[39]
+1.5:10
+letters
+letters[1:3]
+letters[20/2]
+vetor3
+vetor3[vetor3 < 70]
